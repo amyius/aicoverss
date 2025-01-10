fe0d54d8e934c15d4af6f7b474a1e451ba703d32
Amyliu <1787833697@qq.com>
2025-01-10 17:32:05 +0800

    feat 插入广告库

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -97,31 +97,31 @@ class Index extends BaseController
         return view('temporary');
     }
 
-    public function forgotpassword()
-    {
-        if ($this->request->isPost()) {
-            $data = $this->request->post();
-            if (empty($data['name']) || empty($data['password']) || empty($data['resetpassword'])) {
-                return view('forgotpassword', ['error' => '参数错误']);
-            }
+    // public function forgotpassword()
+    // {
+    //     if ($this->request->isPost()) {
+    //         $data = $this->request->post();
+    //         if (empty($data['name']) || empty($data['password']) || empty($data['resetpassword'])) {
+    //             return view('forgotpassword', ['error' => '参数错误']);
+    //         }
 
-            $data['password'] = strtolower($data['password']);
-            $userModel = new \app\model\User();
-            $data['password'] = strtolower($data['password']);
-            $userid = $userModel->where('name', $data['name'])->order('created_at desc')->value('id');
-            if ($userid) {
-                $updatedate = [
-                    'password' => $data['password'],
-                    'updated_at' => date('Y-m-d H:i:s')
-                ];
-                $userModel->where('id', $userid)->save($updatedate);
-                return json(['message' => '密码重置成功,请登录', 'code' => 1]);
-            } else {
-                return json(['message' => '密码重置失败，或者该用户暂无账号，请稍后再试', 'code' => 0]);
-            }
-        }
-        return view('forgotpassword');
-    }
+    //         $data['password'] = strtolower($data['password']);
+    //         $userModel = new \app\model\User();
+    //         $data['password'] = strtolower($data['password']);
+    //         $userid = $userModel->where('name', $data['name'])->order('created_at desc')->value('id');
+    //         if ($userid) {
+    //             $updatedate = [
+    //                 'password' => $data['password'],
+    //                 'updated_at' => date('Y-m-d H:i:s')
+    //             ];
+    //             $userModel->where('id', $userid)->save($updatedate);
+    //             return json(['message' => '密码重置成功,请登录', 'code' => 1]);
+    //         } else {
+    //             return json(['message' => '密码重置失败，或者该用户暂无账号，请稍后再试', 'code' => 0]);
+    //         }
+    //     }
+    //     return view('forgotpassword');
+    // }
 
 
     //记录下载次数
